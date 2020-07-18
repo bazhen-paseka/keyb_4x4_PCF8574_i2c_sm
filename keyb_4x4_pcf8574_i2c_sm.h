3a79fce3306cdae7af1b07077e2d63e1b5f090b7
Бажен Левковець <bazhen.paseka@gmail.com>
2020-07-18 20:35:37 +0300

    add file "local_config.h"

--- keyb_4x4_pcf8574_i2c_sm.h
+++ keyb_4x4_pcf8574_i2c_sm.h
@@ -7,11 +7,7 @@
 	#include <stdio.h>
 	#include "main.h"
 	#include "i2c_techmaker_sm.h"
-
-/******************************************************/
-
-	#define I2C_ADR_PCF_1 0x38
-	#define I2C_ADR_PCF_2 0x20
+	#include "pcf8574_local_config.h"
 
 /******************************************************/
 
@@ -24,14 +20,19 @@
 
 /******************************************************/
 
-	void PCF8574_struct_init 		(PCF8574_Struct * _pcf, uint8_t _channel, I2C_HandleTypeDef * _i2c, UART_HandleTypeDef * _uart, uint8_t _addr)	;
-	void PCF8574_start_keyboard 	(PCF8574_Struct * _pcf)																	;
-	char PCF8574_scan_keyboard 		(PCF8574_Struct * _pcf)																	;
-	void PCF8574_debug_print_key 	(PCF8574_Struct * _pcf, char _key_char)														;
-	void PCF8574_IRQ_enable 		(PCF8574_Struct * _pcf)																	;
-	void PCF8574_update_IRQ_flag	(PCF8574_Struct * _pcf, uint8_t _flag)															;
-	uint8_t PCF8574_get_IRQ_flag	(PCF8574_Struct * _pcf)																	;
-	void PCF8574_set_IRQ_flag 	(uint8_t _channel) ;
+	void PCF8574_struct_init 		(	PCF8574_Struct 		*_pcf,
+										uint8_t 			_channel,
+										I2C_HandleTypeDef	*_i2c,
+										UART_HandleTypeDef	*_uart,
+										uint8_t 			_addr)	;
+
+	void PCF8574_start_keyboard 	(PCF8574_Struct * _pcf)					;
+	char PCF8574_scan_keyboard 		(PCF8574_Struct * _pcf)					;
+	void PCF8574_debug_print_key 	(PCF8574_Struct * _pcf, char _key_char)	;
+	void PCF8574_IRQ_enable 		(PCF8574_Struct * _pcf)					;
+	void PCF8574_update_IRQ_flag	(PCF8574_Struct * _pcf, uint8_t _flag)	;
+	uint8_t PCF8574_get_IRQ_flag	(PCF8574_Struct * _pcf)					;
+	void PCF8574_set_IRQ_flag 		(uint8_t _channel) 						;
 
 /******************************************************/
 
