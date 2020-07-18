2d68aa8c3cbffa91ac8ac263359f9ce0b15088c2
Бажен Левковець <bazhen.paseka@gmail.com>
2020-07-18 18:44:57 +0300

    refactoring
    .
    
    .
    
    
    .

--- keyb_4x4_pcf8574_i2c_sm.h
+++ keyb_4x4_pcf8574_i2c_sm.h
@@ -1,33 +1,27 @@
 #ifndef KEYBOARD_4X4_PCF8574_I2C_SM_H_INCLUDED
 #define KEYBOARD_4X4_PCF8574_I2C_SM_H_INCLUDED
 
+/******************************************************/
+
 	#include <string.h>
 	#include <stdio.h>
 	#include "main.h"
 	#include "i2c_techmaker_sm.h"
 
-/***********************************/
+/******************************************************/
 
-/***********************************/
+	#define I2C_ADR_PCF8574 0x38
 
-	char key[4][4];
-	char DataChar[100];
+/******************************************************/
 
-	typedef struct
-	{
-		UART_HandleTypeDef uart;
-		I2C_HandleTypeDef  i2c;
-		uint8_t devAddr;
-	} keyboard_struct;
+	void PCF8574_struct_init 		(I2C_HandleTypeDef * _i2c, UART_HandleTypeDef * _uart, uint8_t _addr)	;
+	void PCF8574_start_keyboard 	(void)																	;
+	char PCF8574_scan_keyboard 		(void)																	;
+	void PCF8574_debug_print_key 	(char _key_char)														;
+	void PCF8574_IRQ_enable 		(void)																	;
+	void PCF8574_update_IRQ_flag	(uint8_t _flag)															;
+	uint8_t PCF8574_get_IRQ_flag	(void)																	;
 
-	keyboard_struct KEY;
-/***********************************/
-
-	void Init_keyboard_struct(I2C_HandleTypeDef * _i2c, UART_HandleTypeDef * _uart, uint8_t _addr);
-	void Start_keyboard(void);
-	char Scan_keyboard(void);
-	uint8_t Get_keyboard_int_flag(void);
-	void Update_keyboard_int_flag(uint8_t _flag);
-
+/******************************************************/
 
 #endif 	//	KEYBOARD_4X4_PCF8574_I2C_SM_H_INCLUDED
