da1ac7f7f998d197cb5e4ac3ea5727f27beafdfe
Paseka <bazhen.paseka@gmail.com>
2022-10-26 07:09:40 +0300

    no erorors

--- keyb_4x4_pcf8574_i2c_sm.h
+++ keyb_4x4_pcf8574_i2c_sm.h
@@ -6,12 +6,13 @@
 	#include <string.h>
 	#include <stdio.h>
 	#include "main.h"
+	#include "i2c.h"
 	#include "usart.h"
 
-	#include "i2c_techmaker_sm.h"
+//	#include "i2c_techmaker_sm.h"
 	#include "keyb_4x4_pcf8574_i2c_sm.h"
 	#include "pcf8574_local_config.h"
-	#include "3Dbox-keyboards-local-config.h"
+//	#include "3Dbox-keyboards-local-config.h"
 
 /******************************************************/
 
@@ -26,20 +27,19 @@
 
 /******************************************************/
 
-	void PCF8574_struct_init(		PCF8574_Struct 		*_pcf		,
-									I2C_HandleTypeDef	*_i2c		,
-									uint8_t 			_channel	,
-									uint8_t 			_addr		) ;
+	void PCF8574_struct_init		( PCF8574_Struct 		*_pcf		,
+									  I2C_HandleTypeDef	*_i2c		,
+									  uint8_t 			_channel	,
+									  uint8_t 			_addr		) ;
 
-	uint8_t PCF8574_scan_keyboard(	PCF8574_Struct		*_pcf		) ;
+	uint8_t PCF8574_scan_keyboard	( PCF8574_Struct		*_pcf		) ;
 
 
-	void PCF8574_IRQ_enable(		PCF8574_Struct		*_pcf		) ;
-	void PCF8574_update_IRQ_flag(	PCF8574_Struct		*_pcf		,
-									uint8_t				_flag		) ;
+	void PCF8574_IRQ_enable			( PCF8574_Struct		*_pcf		) ;
+	void PCF8574_update_IRQ_flag	( uint8_t				_flag		) ;
 
-	uint8_t PCF8574_get_IRQ_flag(	PCF8574_Struct		*_pcf		) ;
-	void PCF8574_set_IRQ_flag(		uint8_t 			_channel	) ;
+	uint8_t PCF8574_get_IRQ_flag	( void ) ;
+	void PCF8574_set_IRQ_flag		( void ) ;
 
 /******************************************************/
 
